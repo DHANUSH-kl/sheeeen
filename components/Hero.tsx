86b5966f8f969eedfd61395056a054d4210957a2
DHANUSH-KL <dhanuchandu1232@gmail.com>
2026-03-14 02:07:56 +0530

    discoverrrrr

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -105,15 +105,7 @@ export default function Hero() {
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-[#02282C] to-transparent pointer-events-none" />
 
       {/* Scroll Indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 1 }}
-        className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-3"
-      >
-        <span className="font-['Oswald'] text-[10px] tracking-[0.3em] text-white/40 uppercase">Scroll</span>
-        <div className="w-[1px] h-12 bg-gradient-to-b from-white/0 via-white/50 to-white/0" />
-      </motion.div>
+      
     </section>
   );
 }
